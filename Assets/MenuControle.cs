5eb30b32361351b7b314f7dc5061081128ab2f2c
Urii Shalya <D.karaka@protonmail.com>
2020-01-07 20:44:57 +0200

    Initial game commit

---
+++ Assets/MenuControle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuControle : MonoBehaviour {
+    public void PlayPressed()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    public void ExitPressed()
+    {
+        Application.Quit();
+        Debug.Log("Exit pressed!");
+    }
+    public void ExitGamePressed()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+    public void Load2Pressed()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
